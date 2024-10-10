848573f910b57ff021fd2fe8dcf13f7212c0e7d8
minaripenguin <minaripenguin@users.noreply.github.com>
2024-10-10 17:36:07 +0800

    core: Introduce FeatureHooksUtils
    Co-authored-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -121,7 +121,7 @@ import com.android.internal.annotations.GuardedBy;
 import com.android.internal.annotations.Immutable;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.os.SomeArgs;
-import com.android.internal.util.PropImitationHooks;
+import com.android.internal.util.arrow.FeatureHooksUtils;
 import com.android.internal.util.UserIcons;
 
 import dalvik.system.VMRuntime;
@@ -810,8 +810,8 @@ public class ApplicationPackageManager extends PackageManager {
 
     @Override
     public boolean hasSystemFeature(String name, int version) {
-        return PropImitationHooks.hasSystemFeature(name,
-                mHasSystemFeatureCache.query(new HasSystemFeatureQuery(name, version)));
+        boolean hasSystemFeature = mHasSystemFeatureCache.query(new HasSystemFeatureQuery(name, version));
+        return FeatureHooksUtils.hasSystemFeature(name, version, hasSystemFeature);
     }
 
     /** @hide */
