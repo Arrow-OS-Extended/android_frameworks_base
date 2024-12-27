2ae9c3595bb2ea9cebb7002cb39d136fcd84afb4
ZorEl212 <yabsirad212@gmail.com>
2024-12-27 13:22:32 +0000

    core: Fix package path of `FeatureHooksUtils`

--- core/java/com/android/internal/util/arrow/FeatureHooksUtils.java
+++ core/java/com/android/internal/util/arrow/FeatureHooksUtils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.internal.util.android;
+package com.android.internal.util.arrow;
 
 import android.app.ActivityThread;
 import android.os.SystemProperties;
