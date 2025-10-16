3ffe9e3192231ad30a9d2f7925f6a6b0c3de53eb
Rimuru <paleloebarbershop@gmail.com>
2025-10-16 18:04:09 +0000

    PIH: Drop PropImitationHooks
    Change-Id: Icdfecb0ee7b6a74fc0b4d2367b71d6e07048e2f2
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -56,7 +56,6 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
-import com.android.internal.util.PropImitationHooks;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1245,7 +1244,6 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        PropImitationHooks.setProps(context);
         GamesPropsUtils.setProps(context);
         return app;
     }
@@ -1264,7 +1262,6 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        PropImitationHooks.setProps(context);
         GamesPropsUtils.setProps(context);
         return app;
     }
--- keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
+++ keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -75,8 +75,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import com.android.internal.util.PropImitationHooks;
-
 import javax.crypto.SecretKey;
 
 /**
@@ -166,7 +164,6 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
-        PropImitationHooks.onEngineGetCertificateChain();
         KeyEntryResponse response = getKeyMetadata(alias);
 
         if (response == null || response.metadata.certificate == null) {
