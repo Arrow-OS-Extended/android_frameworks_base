2fa2d40bc8b132e94cb42c8c840ea61a7e4b9f9b
Dheeraj R Shrivatsa <dheerajshrivatsa@gmail.com>
2025-11-13 13:30:10 +0530

    Revert "base: Auto set vbmeta digest and other props [1/2]"
    This reverts commit e66a9e1f1b1a8f4e732e0a10c4209906e8ac1365.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -107,8 +107,6 @@ import com.android.internal.widget.ILockSettings;
 import com.android.server.am.ActivityManagerService;
 import com.android.server.ambientcontext.AmbientContextManagerService;
 import com.android.server.appbinding.AppBindingService;
-import com.android.server.arrow.CustomDeviceConfigService;
-import com.android.server.arrow.VbmetaHashService;
 import com.android.server.art.ArtManagerLocal;
 import com.android.server.attention.AttentionManagerService;
 import com.android.server.audio.AudioService;
@@ -2544,11 +2542,6 @@ public final class SystemServer implements Dumpable {
 
             t.traceBegin("StartMediaMetricsManager");
             mSystemServiceManager.startService(MediaMetricsManagerService.class);
-            t.traceEnd();
-
-            // VbmetaHashService
-            t.traceBegin("VbmetaHashService");
-            mSystemServiceManager.startService(VbmetaHashService.class);
             t.traceEnd();
         }
 
